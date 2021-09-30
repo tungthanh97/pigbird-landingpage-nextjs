05f7644ed16e6a8c6fdc7d9844c70fafc76d7f14
ichigofan <tung020197@gmail.com>
2021-09-30 15:02:14 +0700

    fix: add src folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  purge: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: false,
   theme: {
     extend: {
       fontFamily: {
